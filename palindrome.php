adfb5000895130a55ed0802f22c3408e648efdae
Aamil10 <127920055+Aamil10@users.noreply.github.com>
2025-01-09 20:21:39 +0530

    Add files via upload

---
+++ palindrome.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>Palindrome Numbers</title>
+  </head>
+  <body>
+    <h1>Palindrome Check</h1>
+    <form method="POST">
+      Enter the number: <input type="number" name="num1" required><br><br>
+      <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+      $num1 = $_POST['num1'];  // Get the number from the form
+      $reverse = strrev($num1);  // Reverse the number
+
+      if ($num1 == $reverse) {
+        echo "The number is a palindrome.";
+      } else {
+        echo "The number is not a palindrome.";
+      }
+    }
+    ?>
+  </body>
+</html>
